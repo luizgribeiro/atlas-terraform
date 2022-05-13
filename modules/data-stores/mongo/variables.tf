2a5bb89417ea25658a650c0690794665b863ce03
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2022-05-13 17:50:40 -0300

    initial commit - mongo vars added

---
+++ modules/data-stores/mongo/variables.tf
@@ -0,0 +1,11 @@
+variable "mongo_public_key" {
+  description = "MongoDB Atlas public key" 
+  type = string
+  nullable = false
+}
+
+variable "mongo_private_key" {
+  description = "MongoDB Atlas private key" 
+  type = string
+  nullable = false
+}
